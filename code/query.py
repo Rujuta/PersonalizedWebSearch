22ed32285caa1d0c7245e9762b8543476a9276f2
Biman Gujral <biman@Bimans-MacBook-Pro.local>
2014-12-13 01:46:25 -0500

    query generic set

--- code/query.py
+++ code/query.py
@@ -4,29 +4,35 @@ import fileread
 
 query_counts=defaultdict(int)
 query_terms=defaultdict(int)
+query_url_set = defaultdict(set)
 
 """Returns a dictionary of type {<Query_ID>, count} that indicates how many times 
 a particular query has been queried"""
 def get_dict_query_counts(search_logs):
-    global query_counts
     for log in search_logs:
             items=log.split()
             if items[2] == 'Q':
                 count=query_counts[items[4]]
                 count+=1
                 query_counts[items[4]]=count
-
+    return query_counts
 
 """Create a dictionary keyed by queryID and the number of terms in the query"""
 def get_terms_in_query(search_logs):
-    global query_terms
     for log in search_logs:
             items=log.split()
             if items[2] == 'Q':
                 count_query_terms=len(items[5].split(","))
                 query_terms[items[4]]=count_query_terms
+    return query_terms
 
-
+def get_urls_in_query(search_logs):
+    for log in search_logs:
+            items=log.split()
+            if items[2] == 'Q':
+                for each in items[6:]:
+                    query_url_set[items[4]].add(each)
+    return query_url_set
 
 def fill_query_doc_features(query_doc):
     # Fill in the number of times an item has been queried for by anyone and the no of terms in that query
