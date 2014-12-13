ab45e38a6fcaf4cf80cc5c5056f1c63ae7928521
Biman Gujral <biman@Bimans-MacBook-Pro.local>
2014-12-13 18:36:38 -0500

    query try catch

--- code/query.py
+++ code/query.py
@@ -34,15 +34,14 @@ def get_urls_in_query(search_logs):
 
 def fill_query_doc_features(query_terms, query_counts, query_doc):
     # Fill in the number of times an item has been queried for by anyone and the no of terms in that query
-    try:
         for k,v in query_doc.items():
             count_terms= query_terms[k[0]]
             query_doc[k]['terms']=count_terms
-            count_queried_for=query_counts[k[0]]
-            query_doc[k]['frequency']=count_queried_for
-        except KeyError:
-            query_doc[k]['terms']=0
-            query_doc[k]['frequency']=0
+            try:
+                count_queried_for=query_counts[k[0]]
+                query_doc[k]['frequency']=count_queried_for
+            except KeyError:
+                query_doc[k]['frequency']=0
         return query_doc
 
 
